b59bdcca2eb1e06acbbf4ec283ea11b1db374b01
José Antonio Murillo <josemurillo@fb.com>
2019-07-19 10:48:54 -0700

    when adding an event, user can open a view controller where friends will show up

---
+++ hangouts/View Controllers/Map/FriednsToViewController.m
@@ -0,0 +1,34 @@
+//
+//  FriednsToViewController.m
+//  hangouts
+//
+//  Created by josemurillo on 7/19/19.
+//  Copyright © 2019 nev. All rights reserved.
+//
+
+#import "FriednsToViewController.h"
+
+@interface FriednsToViewController ()
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
+@end
+
+@implementation FriednsToViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
