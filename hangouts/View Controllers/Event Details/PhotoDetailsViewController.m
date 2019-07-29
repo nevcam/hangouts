d18fe05273abb05cf100a4ec862c02bf4cee7adc
José Antonio Murillo <josemurillo@fb.com>
2019-07-29 15:42:17 -0700

    Users can see when the photo was posted

--- hangouts/View Controllers/Event Details/PhotoDetailsViewController.m
+++ hangouts/View Controllers/Event Details/PhotoDetailsViewController.m
@@ -12,6 +12,7 @@
 
 @property (weak, nonatomic) IBOutlet UIImageView *photoView;
 @property (weak, nonatomic) IBOutlet UIButton *downloadButton;
+@property (weak, nonatomic) IBOutlet UILabel *dateView;
 
 @end
 
@@ -33,12 +34,58 @@
         }
         self.photoView.image = [UIImage imageWithData:data];
     }];
+    self.dateView.text = [self getDate];
 }
 
 #pragma mark - Download Image
 
 - (IBAction)clickedDownload:(id)sender {
     UIImageWriteToSavedPhotosAlbum(self.photoView.image, nil, nil, nil);
+}
+
+#pragma mark - Delete Image
+
+- (IBAction)clickedDelete:(id)sender {
+}
+
+#pragma mark - Customize Date
+- (NSString *)getDate {
+    
+    NSDate *createdAt = [self.photoObject createdAt];
+    NSDate *todayDate = [NSDate date];
+    double ti = [createdAt timeIntervalSinceDate:todayDate];
+    ti = ti * -1;
+    
+    if(ti < 1) {
+        return @"never";
+    } else  if (ti < 60) {
+        return @"less than a minute ago";
+    } else if (ti < 120) {
+        int diff = round(ti / 60);
+        return [NSString stringWithFormat:@"%d minute ago", diff];
+    } else if (ti < 3600) {
+        int diff = round(ti / 60);
+        return [NSString stringWithFormat:@"%d minutes ago", diff];
+    } else if (ti < 7200) {
+        int diff = round(ti / 60 / 60);
+        return [NSString stringWithFormat:@"%d hour ago", diff];
+    } else if (ti < 86400) {
+        int diff = round(ti / 60 / 60);
+        return [NSString stringWithFormat:@"%d hours ago", diff];
+    } else if (ti < 172800) {
+        int diff = round(ti / 60 / 60 / 24);
+        return [NSString stringWithFormat:@"%d days ago", diff];
+    } else if (ti < 2629743) {
+        int diff = round(ti / 60 / 60 / 24);
+        return [NSString stringWithFormat:@"%d days ago", diff];
+    } else {
+        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+        formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
+        formatter.dateStyle = NSDateFormatterShortStyle;
+        formatter.timeStyle = NSDateFormatterNoStyle;
+        // Convert Date to String
+        return [formatter stringFromDate:createdAt];
+    }
 }
 
 @end
