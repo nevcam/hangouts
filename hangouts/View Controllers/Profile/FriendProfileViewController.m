9c959a606a9654a963e49dff7569468535f43a2e
José Antonio Murillo <josemurillo@fb.com>
2019-07-29 18:21:33 -0700

    When users are searching for friends, they can see complete profiles by clicking on profile picture

---
+++ hangouts/View Controllers/Profile/FriendProfileViewController.m
@@ -0,0 +1,32 @@
+//
+//  FriendProfileViewController.m
+//  hangouts
+//
+//  Created by josemurillo on 7/29/19.
+//  Copyright © 2019 nev. All rights reserved.
+//
+
+#import "FriendProfileViewController.h"
+
+@interface FriendProfileViewController ()
+
+@end
+
+@implementation FriendProfileViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
