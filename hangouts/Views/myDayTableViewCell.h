5a8112e33b8b40ba6a95a310059892fcdd27b6a2
José Antonio Murillo <josemurillo@fb.com>
2019-08-05 16:42:06 -0700

    Gave functionality to my hangouts counters and my day cells

---
+++ hangouts/Views/myDayTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  myDayTableViewCell.h
+//  hangouts
+//
+//  Created by josemurillo on 8/5/19.
+//  Copyright © 2019 nev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface myDayTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *eventNameLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
