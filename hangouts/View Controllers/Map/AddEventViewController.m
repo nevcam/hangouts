b0848348de344d617f0a80af65a71aa647412c16
Sandra Roman Rivera <sroman98@fb.com>
2019-08-07 10:53:35 -0700

    Fixed 'request entity too large' when adding event, removed unnecessary nslogs

--- hangouts/View Controllers/Map/AddEventViewController.m
+++ hangouts/View Controllers/Map/AddEventViewController.m
@@ -303,7 +303,7 @@
     [self.mapView addAnnotation:annotation];
 }
 
-#pragma mark - Can Edit/Add Profile
+#pragma mark - Event photo methods
 
 - (IBAction)clickedAddPhoto:(id)sender {
     UIImagePickerController *imagePickerVC = [UIImagePickerController new];
@@ -321,10 +321,23 @@
 }
 
 - (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    UIImage *const originalImage = info[UIImagePickerControllerOriginalImage];
-    _eventPhoto.image = originalImage;
+    UIImage *editedImage = info[UIImagePickerControllerEditedImage];
+    UIImage *resizedImage = [self resizeImage:editedImage withSize:CGSizeMake(350, 350)];
+    _eventPhoto.image = resizedImage;
     
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 #pragma mark - Edit Event Setup Methods
